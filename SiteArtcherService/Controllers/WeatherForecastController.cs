6f1496bd0274cd03772eb1e95275cec23f0806ef
thais-ads <thaiss.spinoza@gmail.com>
2023-12-17 21:18:52 -0300

    deixando o codigo mais alteravel

---
+++ SiteArtcherService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SiteArcherService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+
+    }
+}
